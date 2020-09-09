24525511ebeccecc5c3f178d8316f3015e0c4147
Alex <alexnoir03@gmail.com>
2020-09-10 00:37:05 +0100

    Initial commit

---
+++ Source/Quincy/Program.cs
@@ -0,0 +1,13 @@
+﻿using Quincy.DebugUtils;
+using System;
+
+namespace Quincy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logging.Log("Hello World!");
+        }
+    }
+}
